09fdcbf2a12722c37e6fc4ec4549814a195a5b38
sahoosunilkumar <sahoosunilkumar@gmail.com>
2019-05-17 14:06:13 +0530

    bumping submodules

--- update_submodules.sh
+++ update_submodules.sh
@@ -3,6 +3,8 @@
 #Set whatever number of arguments you expect for the Java jar you have
 ARGS_EXPECTED=1
 
+shouldPush=true
+
 if [[ $# == ${ARGS_EXPECTED} ]]
 then
 currentBranch=`git rev-parse --abbrev-ref HEAD`
@@ -19,13 +21,22 @@ submoduleConfigUpdate="git config -f .gitmodules submodule.$OUTPUT.branch $branc
 eval ${submoduleConfigUpdate}
 done
 
-submoduleUpdateCommand="git submodule update --remote --merge --recursive"
-eval echo ${submoduleUpdateCommand}
+submoduleUpdateCommand="git submodule update --remote --merge --recursive|| echo \"Error\""
+updateResult=$(eval ${submoduleUpdateCommand})
+if [[ ${updateResult} == *Error* ]]; then
+  shouldPush=false
+  echo ${updateResult}
+  else
+  shouldPush=true
+fi
 
-if [[ "$1" = "-push" ]]
+if [[ ("$1" = "-push") && (${shouldPush}=true) ]]
 then
 codePushCommand="git add --all && git commit -m 'bumping submodules' && git push origin $currentBranch"
 eval ${codePushCommand}
+echo "SUCCESS"
+else
+echo "Error"
 fi
   exit 0
 else
